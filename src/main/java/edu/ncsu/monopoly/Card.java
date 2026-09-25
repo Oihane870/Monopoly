f7e838182726fa1c35d57f46740b981f3e87f96b
1159878 <1159878@U032831.adm.ehu.es>
2026-09-25 14:31:51 +0200

    card updated

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,7 +1,7 @@
 package edu.ncsu.monopoly;
 //Eguneraketa irailak 25 bigarrena
 public abstract class Card {
-
+//actualizacion viernes 25 is2
     public static final int TYPE_CHANCE = 1;
     public static final int TYPE_CC = 2;
 
